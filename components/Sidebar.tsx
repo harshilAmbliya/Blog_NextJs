1f55c7edb4f0f6e76326d64c6727417fbec660ce
priyankambliya <priyankambliya18@gmail.com>
2023-09-04 19:04:11 +0530

    feat:ui changes

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,29 +1,14 @@
-import { ScrollArea } from "./ui/scroll-area";
+"use client";
+
+import React, { useState } from "react";
 
 type Props = {};
 
 const Sidebar = (props: Props) => {
-  return (
-    <ScrollArea className="h-screen  w-[350px] bg-[#143a3a]">
-      <ul className="px-2 py-3">
-        <li className="bg-green-100 px-3 py-2 rounded-full text-center shadow-md my-5 text-violet-400">
-          Home
-        </li>
-        <li className="bg-green-100 px-3 py-2 rounded-full text-center shadow-md my-5 text-violet-400">
-          AbOut
-        </li>
-        <li className="bg-green-100 px-3 py-2 rounded-full text-center shadow-md my-5 text-violet-400">
-          Contact
-        </li>
-        <li className="bg-green-100 px-3 py-2 rounded-full text-center shadow-md my-5 text-violet-400">
-          Blog
-        </li>
-        <li className="bg-green-100 px-3 py-2 rounded-full text-center shadow-md my-5 text-violet-400">
-          Suggestions
-        </li>
-      </ul>
-    </ScrollArea>
-  );
+  const [open, setOpen] = useState(false);
+  const toggleSidebar = () => {
+    setOpen(!open);
+  };
+  return <div className={`${open ? "w-64" : "w-0"} bg-blue-200`}></div>;
 };
-
 export default Sidebar;
